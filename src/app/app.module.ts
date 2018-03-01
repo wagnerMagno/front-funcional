b97da189fca3fc78bab6b06c49a24dec7b3210ae
wagnerMagno <wagner.sales@ccc.ufcg.edu.br>
2018-02-28 22:44:29 -0300

    Ajustando Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SubmitArquivoComponent } from './submit-arquivo/submit-arquivo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubmitArquivoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/submit-arquivo/submit-arquivo.component.ts
@@ -0,0 +1,10 @@
+import{Component} from '@angular/core';
+import { p } from '@angular/core/src/render3';
+
+
+
+@Component({
+    selector: 'submit-arquivo-component',
+    templateUrl: 'submit-arquivo.html'
+    })
+export class SubmitArquivoComponent {}
