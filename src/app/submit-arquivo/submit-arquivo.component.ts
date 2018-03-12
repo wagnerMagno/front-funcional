2d33c8d08e3e85be918e6768bacb2f04b55202af
Rafael Klynger <rafafhgkifh@gmail.com>
2018-03-12 08:56:00 -0300

    Corrections to requests.

--- src/app/submit-arquivo/submit-arquivo.component.ts
+++ src/app/submit-arquivo/submit-arquivo.component.ts
@@ -1,49 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {Http, RequestOptions} from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 
 @Component({
-  selector: 'app-submit-arquivo',
-  templateUrl: './submit-arquivo.component.html',
-  styleUrls: ['./submit-arquivo.component.css']
+    selector: 'app-submit-arquivo',
+    templateUrl: './submit-arquivo.component.html',
+    styleUrls: ['./submit-arquivo.component.css']
 })
 export class SubmitArquivoComponent implements OnInit {
 
-  dadosTarefa: any = {
-    studentSubmission: '',
-    studentId: '',
-    listNumber: ''
-  }
+    dadosTarefa: any = {
+        studentSubmission: '',
+        studentId: '',
+        listNumber: ''
+    };
+    data: FormData;
 
-  
-  constructor(private http: HttpClient) { }
-  
-  ngOnInit() {
-  }
 
-   submitDate = new FormData();
+    constructor(private http: HttpClient) {
+        this.data = new FormData();
 
-  submissaoArquivo(){
-    console.log("fom >>>>", this.dadosTarefa);
-    this.submitDate.set("studentSubmission", this.dadosTarefa.studentSubmission);
-      this.submitDate.set("studentId", this.dadosTarefa.studentId);
-      this.submitDate.set("listNumber", this.dadosTarefa.listNumber);
-      
-      console.log("studentSubmission >>>>", this.submitDate.get("studentSubmission"));
-      console.log("studentId >>>>", this.submitDate.get("studentId"));
-      console.log("listNumber >>>>", this.submitDate.get("listNumber"));
-
-    if(this.dadosTarefa.studentId && this.dadosTarefa.studentSubmission && this.dadosTarefa.listNumber){
-      
-    
-      
-      this.http.post("http://localhost:3000/submission", this.dadosTarefa)
-        .subscribe(response => {
-          console.log("reponse ", response);
-        }, erro => {
-          console.log(erro);
-        });
     }
-  }
+
+    ngOnInit() {
+    }
+
+    getFile(event) {
+        this.dadosTarefa.studentSubmission = event.target.files[0];
+    }
+
+
+    submissaoArquivo() {
+        this.data.set('studentSubmission', this.dadosTarefa.studentSubmission);
+        this.data.set('studentId', this.dadosTarefa.studentId);
+        this.data.set('listNumber', this.dadosTarefa.listNumber);
+
+        const headers = new HttpHeaders();
+
+        headers.set('Accept', 'application/json');
+        headers.delete('Content-Type');
+        // headers.set('Content-Type', 'multipart/form-data');
+
+        if (this.dadosTarefa.studentId && this.dadosTarefa.studentSubmission && this.dadosTarefa.listNumber) {
+
+            console.log('adfasd', this.dadosTarefa);
+            this.http.post('http://localhost:3000/submission', this.data, { headers })
+                .subscribe(response => {
+                    console.log('reponse ', response);
+                }, erro => {
+                    console.log(erro);
+                });
+        }
+    }
 
 }
--- src/app/visualizar-resultado/visualizar-resultado.component.ts
+++ src/app/visualizar-resultado/visualizar-resultado.component.ts
@@ -1,48 +1,51 @@
-import { Component, OnInit , Input, Output} from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'app-visualizar-resultado',
-  templateUrl: './visualizar-resultado.component.html',
-  styleUrls: ['./visualizar-resultado.component.css']
+    selector: 'app-visualizar-resultado',
+    templateUrl: './visualizar-resultado.component.html',
+    styleUrls: ['./visualizar-resultado.component.css']
 })
 export class VisualizarResultadoComponent implements OnInit {
 
-  @Output()  result : any = {
-    listName : "",
-    studentId : "",
-    exceptions : "",
-    passed : "",
-    failed : "",
-    total : ""
-  };
-  constructor( private http: HttpClient) { }
+    @Output() result: any = {
+        listName: '',
+        studentId: '',
+        exceptions: '',
+        passed: '',
+        failed: '',
+        total: ''
+    };
 
-  ngOnInit() {
-     
-  }
+    dadosTarefa: any = {
+        studentId: '',
+        listNumber: ''
+    };
+    constructor(private http: HttpClient) { }
 
-  dadosTarefa: any = {
-    studentId: '',
-    listNumber: ''
-  }
-
-
-  getResultado(){
-    if(this.dadosTarefa.studentId && this.dadosTarefa.listNumber){
-
-      this.http.get('http://localhost:3000/submission?studentId=' 
-          + this.dadosTarefa.studentId+ '&listName=' + this.dadosTarefa.listNumber)
-      .map(res => res)
-      .subscribe(dados => {
-        console.log(dados);
-        this.result = dados;
-      }, erro => {
-        console.log(erro);
-      });
+    ngOnInit() {
 
     }
-  }  
+
+
+
+    getResultado() {
+        if (this.dadosTarefa.studentId && this.dadosTarefa.listNumber) {
+
+            this.http.get('http://localhost:3000/submission?studentId='
+                + this.dadosTarefa.studentId + '&listName=' + this.dadosTarefa.listNumber)
+                .map(res => res)
+                .subscribe((response: any) => {
+                    console.log(response);
+                    if (response.submission.length > 0) {
+                        this.result = response.submission[0];
+                    }
+                }, erro => {
+                    console.log(erro);
+                });
+
+        }
+    }
 
 }
